51fd9f6c06ae8d4e1925ef807228737e153ae866
RoXSoURcE <88581425+RoXSoURcE@users.noreply.github.com>
2021-08-12 23:19:48 +0300

    Add files via upload

--- RoX.lua
+++ RoX.lua
@@ -11115,7 +11115,6 @@ Text = [[
 keyboard = {} 
 keyboard.inline_keyboard = {
 {{text = '᥀ Developer',url="https://t.me/BBB43"}},
-{{text = '᥀ Developer',url="t.me/BBB43"}},
 }
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..TokenBot..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/BBB43&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
